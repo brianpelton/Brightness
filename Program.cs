f055f3cd89f425d8ef18dc7e85776685b288f93c
brianpelton <brian.pelton@gmail.com>
2015-04-26 16:25:31 -0700

    First checkin.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Brightness
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Pass in brightness change amount or value.");
+                return;
+            }
+
+            var value = args[0];
+            var currentBrightness = WmiFunctions.GetBrightnessLevel();
+
+            Console.WriteLine("Current Brightness:  {0}", currentBrightness);
+            if (value.Contains("+"))
+            {
+                // incremental change
+                try
+                {
+                    var x = int.Parse(value.Replace("+", string.Empty));
+                    WmiFunctions.SetBrightnessLevel(Math.Min(currentBrightness + x, 100));
+                }
+                catch (Exception)
+                {
+                }
+            }
+            else if (value.Contains("-"))
+            {
+                // decremental change
+                try
+                {
+                    var x = int.Parse(value.Replace("-", string.Empty));
+                    WmiFunctions.SetBrightnessLevel(Math.Max(currentBrightness - x, 1));
+                }
+                catch (Exception)
+                {
+                }
+            }
+            else
+            {
+                try
+                {
+                    var x = int.Parse(value);
+                    WmiFunctions.SetBrightnessLevel(x);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ WmiFunctions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Management;
+
+namespace Brightness
+{
+    /// <summary>
+    ///     Functions to modify system values using WMI.
+    /// </summary>
+    internal static class WmiFunctions
+    {
+
+        /// <summary>
+        ///     Query the brightness setting of the display.
+        /// </summary>
+        internal static int GetBrightnessLevel()
+        {
+            try
+            {
+                var s = new ManagementScope("root\\WMI");
+                var q = new SelectQuery("WmiMonitorBrightness");
+                var mos = new ManagementObjectSearcher(s, q);
+                var moc = mos.Get();
+
+                foreach (var managementBaseObject in moc)
+                {
+                    foreach (var o in managementBaseObject.Properties)
+                    {
+                        if (o.Name == "CurrentBrightness")
+                            return Convert.ToInt32(o.Value);
+                    }
+                }
+
+                moc.Dispose();
+                mos.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return 0;
+        }
+
+
+        /// <summary>
+        ///     Change the brightness setting of the display.
+        /// </summary>
+        /// <param name="brightnessLevel">
+        ///     The brightness value to apply. Should
+        ///     be between 0 and 100.
+        /// </param>
+        internal static void SetBrightnessLevel(int brightnessLevel)
+        {
+            if (brightnessLevel < 0 ||
+                brightnessLevel > 100)
+                throw new ArgumentOutOfRangeException("brightnessLevel");
+
+            try
+            {
+                var s = new ManagementScope("root\\WMI");
+                var q = new SelectQuery("WmiMonitorBrightnessMethods");
+                var mos = new ManagementObjectSearcher(s, q);
+                var moc = mos.Get();
+
+                foreach (var managementBaseObject in moc)
+                {
+                    var o = (ManagementObject) managementBaseObject;
+                    o.InvokeMethod("WmiSetBrightness", new object[]
+                    {
+                        UInt32.MaxValue,
+                        brightnessLevel
+                    });
+                }
+
+                moc.Dispose();
+                mos.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
